201e742efac826eb0e20d3ca8de906ca7eb2cb5d
fran <fasensior@users.noreply.github.com>
2024-04-24 10:56:09 +0200

    aaa
    aaaa

--- scipy/_lib/array_api_compat/tests/_helpers.py
+++
@@ -1,8 +0,0 @@
-from importlib import import_module
-
-import pytest
-
-def import_(library):
-    if 'cupy' in library:
-        return pytest.importorskip(library)
-    return import_module(library)
